33f63d9385941380ec1d09aa7b93c8c878a1deda
royno <royno@wix.com>
2021-11-15 18:31:38 +0200

    more changes in ex2 myshell

--- ex_2/myshell.c
+++ ex_2/myshell.c
@@ -3,13 +3,52 @@
 #include <string.h>
 #include <unistd.h>
 #include <errno.h>
+#include <fcntl.h>
+#include <signal.h>
 
 //
 // Created by Roy Noyman on 11/11/2021.
 //
+// reference - rec3
+
+void terminate_signal_handler() {
+    printf("handling termination signal");
+}
+
+void child_signal_handler() {
+    printf("handling child signal");
+}
+
+
+void register_signal_handling(int signum) {
+    struct sigaction new_action;
+    memset(&new_action, 0, sizeof(new_action));
+    if (signum == SIGINT) {
+        new_action.sa_sigaction = terminate_signal_handler;
+        new_action.sa_flags = SA_RESTART;
+    } else if (signum == SIGCHLD) {
+        new_action.sa_sigaction = child_signal_handler;
+        new_action.sa_flags = SA_NOCLDWAIT;
+    } else if (signum = 5) { //reset to default for pipe and redirect
+        new_action.sa_handler = SIG_DFL;
+        new_action.sa_flags = SA_RESTART;
+    }
+    if (sigaction(signum, &new_action, NULL) == -1) {
+        perror("ERROR: SIGNAL HANDLER FAILURE ");
+        exit(1);
+    }
+}
+
+void check_fork(pid_t pid) {
+    if (pid < 0) {
+        perror("ERROR: FORK FAILURE");
+        exit(0);
+    }
+}
+
 int prepare(void) {
-    printf("Prepare your sheel");
-    // What do we want to check before? To be completed/removed
+    register_signal_handling(SIGINT);
+    register_signal_handling(SIGCHLD);
     return 0;
 }
 
@@ -24,44 +63,114 @@ int contains_special_character_at_index(int count, char **arglist) {
         }
     }
     if (strcmp(arglist[count - 1], '&')) {
+        arglist[count - 1] = NULL;
         return count - 1;
     }
     return 0;
 }
 
-int process_arglist(int count, char **arglist) {
-    int special_character_index = contains_special_character_at_index(count, arglist);
-    if (special_character_index == 0) {
-        pid_t pid = fork();
-        if (pid < 0) {
-            perror("ERROR: FORK FAILURE");
+int exec_with_pipe(char **arglist, int index) {
+    char **arglist_part_a = arglist;
+    char **arglist_part_b = arglist + index + 1;
+    int pipefds[2];
+    if (pipe(pipefds) == -1) {
+        perror("ERROR: PIPE FAILURE");
+        exit(0);
+    }
+    int readerfd = pipefds[0];
+    int writerfd = pipefds[1];
+    pid_t pid_1 = fork();
+    check_fork(pid_1);
+    pid_t pid_2 = fork();
+    check_fork(pid_2);
+    if (pid_1 == 0) {
+        register_signal_handling(5);
+        close(readerfd);
+        if ((dup2(writerfd, STDOUT_FILENO) == -1) || (errno == EINTR)) {
+            perror("ERROR DUP2 OF PID_1");
             exit(1);
         }
-        if (pid == 0) {
-            if(execvp(arglist[0], arglist)==-1){
-                exit(errno);
-            }
+        close(writerfd);
+        execvp(arglist_part_a[0], arglist_part_a);
+        perror(arglist_part_a[0]);
+        exit(1);
+    } else if (pid_2 == 0) {
+        register_signal_handling(5);
+        close(writerfd);
+        if ((dup2(readerfd, STDIN_FILENO) == -1) || (errno == EINTR)) {
+            perror("ERROR DUP2 OF PID_2");
+            exit(1);
         }
-        else{
-            waitpid(pid, )
+        close(readerfd);
+        execvp(arglist_part_b[0], arglist_part_b);
+        perror(arglist[0]);
+        exit(1);
+    } else {
+        close(readerfd);
+        close(writerfd);
+        waitpid(pid_1, NULL, WUNTRACED);
+        waitpid(pid_2, NULL, WUNTRACED);
+        return 1;
+    }
+}
+
+int exec_with_redirecting(char **arglist, int index) {
+    int fd = open(arglist[index - 1], O_WRONLY | O_CREAT, O_APPEND);
+    arglist[index - 2] = NULL;
+    pid_t pid = fork();
+    check_fork(pid);
+    if (pid == 0) { //child
+        if ((dup2(STDOUT_FILENO, fd) == -1) || (errno == EINTR)) {
+            perror("ERROR DUP2 OF PID");
+            exit(1);
+        }
+        execvp(arglist[0], arglist);
+        close(fd);
+        perror(arglist[0]);
+        exit(1);
+    } else {
+        waitpid(pid, NULL, WUNTRACED);
+        close(fd);
+    }
+    return 1;
+}
+
+
+int process_arglist(int count, char **arglist) {
+    int special_character_index = contains_special_character_at_index(count, arglist);
+    if (special_character_index == 0) { //no special character
+        pid_t pid = fork();
+        check_fork(pid);
+        if (pid == 0) {
+            register_signal_handling(5);
+            if (execvp(arglist[0], arglist) == -1) {
+                perror("ERROR: EXECVP FAILURE");
+                exit(1);
+            }
+        } else {
+            waitpid(pid, NULL, WUNTRACED); //in order to stop waiting WUNTRACED collect the status of child.
+            return 1;
         }
     }
     char special_char = arglist[special_character_index];
-    if (special_char == 'NULL') { //means pipe
-
-    } else if (special_char == '&') {
-
+    if (special_character_index == count - 1) { // means '&'
+        pid_t pid = fork();
+        check_fork(pid);
+        if (pid == 0) {
+            if (execvp(arglist[0], arglist) == -1) {
+                perror("ERROR: EXECVP FAILURE");
+                exit(1);
+            }
+        }
+        return 1;
+    } else if (special_char == 'NULL') { //means '|'
+        return exec_with_pipe(arglist, special_character_index);
     } else { //means >
-
+        exec_with_redirecting(arglist, count);
     }
-    return 0;
+    return 1;
 }
 
-int main(void) {
-    char *check[] = {{"abc", "|", "bla"},
-                     {"cde", "gfgf"}};
-    printf("%s", check[1]);
-    if (strchr(check[1], '|')) {
-        printf("bla");
-    }
+int finalize() {
+    return 0;
 }
