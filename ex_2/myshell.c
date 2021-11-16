a69312d073ade9f4bbcabd7e8230097154ee94ab
royno <royno@wix.com>
2021-11-16 18:17:35 +0200

    return sichld

--- ex_2/myshell.c
+++ ex_2/myshell.c
@@ -49,7 +49,7 @@ void check_fork(pid_t pid) {
 
 int prepare(void) {
 //    register_signal_handling(SIGINT);
-//    register_signal_handling(SIGCHLD);
+    register_signal_handling(SIGCHLD);
 //    printf("done w prepare\n");
     return 0;
 }
