fff235152f47621ddc9ec3badc452ffb542b6353
royno <royno@wix.com>
2021-11-16 14:46:09 +0200

    bla

--- ex_2/myshell.c
+++ ex_2/myshell.c
@@ -160,13 +160,17 @@ int exec_with_redirecting(char **arglist, int index) {
 int process_arglist(int count, char **arglist) {
     int special_character_index = contains_special_character_at_index(count, arglist);
     int wait_status;
+    int i;
+    printf("%d\n",count);
+    for (i=0; i<count; i++){
+        printf("%s\n",arglist[i]);
+    }
     if (special_character_index == 0) { //no special character
         pid_t pid = fork();
 //        check_fork(pid);
         if (pid == 0) {
             printf("im son proccess\n");
 //            register_signal_handling(5);
-            printf("%s\n",arglist[1]);
             if (execvp(arglist[0], arglist) == -1) {
                 printf("execvp problem\n");
                 fprintf(stderr, "ERROR: EXECVP FAILURE: %s", strerror(errno));
