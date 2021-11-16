e76bd5bc677f026751fa796e927a7144f2248d07
royno <royno@wix.com>
2021-11-16 14:53:37 +0200

    gfd

--- ex_2/myshell.c
+++ ex_2/myshell.c
@@ -60,7 +60,7 @@ void check_fork(pid_t pid) {
 
 int prepare(void) {
 //    register_signal_handling(SIGINT);
-    printf("do prepare");
+    printf("do prepare\n");
 //    register_signal_handling(SIGCHLD);
     return 0;
 }
@@ -171,6 +171,7 @@ int process_arglist(int count, char **arglist) {
 //        check_fork(pid);
         if (pid == 0) {
             printf("im son proccess\n");
+            printf("%s%d%s%d\n","ppid: ",getpid()," ppid: ",getppid())
 //            register_signal_handling(5);
             if (execvp(bla[0], bla) == -1) {
                 printf("execvp problem\n");
