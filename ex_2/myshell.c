cc94da6505dca5538a053948fe678fb0915dd8f2
royno <royno@wix.com>
2021-11-18 11:49:23 +0200

    bla bla

--- ex_2/myshell.c
+++ ex_2/myshell.c
@@ -21,7 +21,7 @@ int register_signal_handling(int signum) {
     memset(&new_action, 0, sizeof(new_action));
     if (signum == SIGINT) {
         printf("we are building sigint sig handler\n");
-        new_action.sa_handler = terminate_signal_handler;
+        new_action.sa_handler = SIG_IGN;
         new_action.sa_flags = SA_RESTART; //Deal with EINTER
     } else if (signum == SIGCHLD) {
         printf("we are building SIGCHLD sig handler\n");
