233401764053b671fefc79f2147f62408de6a04f
royno <royno@wix.com>
2021-12-09 11:52:53 +0200

    documentation and linting to message_sender.c

--- ex_3/message_sender.c
+++ ex_3/message_sender.c
@@ -13,30 +13,29 @@
 
 
 int main(int argc, char *argv[]) {
-    if (argc != 4) {
-        fprintf(stderr, "INVALID NUMBER OF INPUTS");
+    if (argc != 4) { //validate number of args
+        perror("ERROR INVALID NUMBER OF INPUTS");
         exit(1);
     }
-    printf("%s\n",argv[1]);
-    int file_path = open(argv[1], O_RDWR);
+    int file_path = open(argv[1], O_RDWR); //open message slot device file
     if (file_path < 0) {
-        printf("%d",file_path);
-        fprintf(stderr, "INVALID FILE PATH");
+        printf("%d", file_path);
+        perror("ERROR INVALID FILE PATH");
         exit(1);
     }
     unsigned long channel_id = atoi(argv[2]);
-    int set_channel_id = ioctl(file_path, MSG_SLOT_CHANNEL, channel_id);
+    int set_channel_id = ioctl(file_path, MSG_SLOT_CHANNEL, channel_id); //set channel_id to the specified id
     if (set_channel_id != 0) {
-        fprintf(stderr, "ERROR IN SETTING FILE CHANNEL ID");
+        perror("ERROR IN SETTING FILE CHANNEL ID");
         exit(1);
     }
     char *message = argv[3];
     int message_length = strlen(message);
-    int write_message = write(file_path, message, message_length);
-    if(write_message != message_length){
-        fprintf(stderr, "ERROR IN WRITE MESSAGE");
+    int write_message = write(file_path, message, message_length); //write messge to device file
+    if (write_message != message_length) {
+        perror("ERROR IN WRITE MESSAGE");
         exit(1);
     }
     close(file_path);
-    return 0;
+    exit(0);
 }
